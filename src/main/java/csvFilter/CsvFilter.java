dfa6e516e70c3648197776d67ef83e5884c4add6
JesusArtiles <jesusartilescidoncha@gamil.com>
2020-09-30 14:22:11 +0100

    Now working with more than one invoice line

--- src/main/java/csvFilter/CsvFilter.java
+++ src/main/java/csvFilter/CsvFilter.java
@@ -2,33 +2,37 @@ package csvFilter;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 
 
 
 
 public class CsvFilter {
+
+    private String decimalRegex = "\\d+(\\.\\d+)?";
+    private String cifRegex = "^[a-zA-Z]{1}\\d{7}[a-zA-Z0-9]{1}$";
+    private String nifRegex = "^\\d{8}[a-zA-Z]{1}$";
+
     public List<String> filter(List<String> lines){
         List result = new ArrayList<String>();
         result.add(lines.get(0));
-        String invoice = lines.get(1);
-        String[] fields = invoice.split(",");
 
-        String ivaField = fields[4];
-        String igicField = fields[5];
-        String cifField = fields[7];
-        String nifField = fields[8];
+        for(int i = 1; i < lines.size();i++){
+            String invoice = lines.get(i);
+            String[] fields = invoice.split(",");
 
-        String decimalRegex = "\\d+(\\.\\d+)?";
-        String cifRegex = "^[a-zA-Z]{1}\\d{7}[a-zA-Z0-9]{1}$";
-        String nifRegex = "^\\d{8}[a-zA-Z]{1}$";
+            String ivaField = fields[4];
+            String igicField = fields[5];
+            String cifField = fields[7];
+            String nifField = fields[8];
 
-        Boolean taxFieldsAreMutuallyExclusive = (ivaField.matches(decimalRegex) || igicField.matches(decimalRegex)) && (ivaField.isEmpty() || igicField.isEmpty());
-        Boolean idFieldsAreMutuallyExclusive = (cifField.matches(cifRegex) || nifField.matches(nifRegex)) && ((cifField.isEmpty() || nifField.matches("\\s")));
+            Boolean taxFieldsAreMutuallyExclusive = (ivaField.matches(decimalRegex) || igicField.matches(decimalRegex)) && (ivaField.isEmpty() || igicField.isEmpty());
+            Boolean idFieldsAreMutuallyExclusive = (cifField.matches(cifRegex) || nifField.matches(nifRegex)) && ((cifField.isEmpty() || nifField.matches("\\s")));
 
-        if (taxFieldsAreMutuallyExclusive && idFieldsAreMutuallyExclusive) {
-            result.add(lines.get(1));
+            if (taxFieldsAreMutuallyExclusive && idFieldsAreMutuallyExclusive) {
+                result.add(lines.get(i));
+            }
         }
+
         return result;
     }
 }
--- src/test/java/CsvFilterShould.java
+++ src/test/java/CsvFilterShould.java
@@ -1,6 +1,7 @@
 import csvFilter.CsvFilter;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -56,11 +57,20 @@ public class CsvFilterShould {
     }
 
     @Test
-    public void exclude_lines_with_both_Id_fields_populated(){
+    public void exclude_lines_with_both_Id_fields_populated_as_they_are_exclusive(){
         String invoiceLine ="1,02/05/2019,1000,810,,12,ACER Laptop,B76430134,45777307Q";
 
         List<String> result = new CsvFilter().filter(Arrays.asList(headerLine, invoiceLine));
         assertEquals(Arrays.asList(headerLine),result);
+    }
+    @Test
+    public void allow_for_more_than_one_line(){
+        String invoiceLine ="1,02/05/2019,1000,810,,12,ACER Laptop,B76430134, ";
+        String invoiceLine2 ="2,02/05/2019,1000,810,,12,ACER Laptop,,45777307Q";
+
+        List<String> result = new CsvFilter().filter(Arrays.asList(headerLine, invoiceLine, invoiceLine2));
+        assertEquals(Arrays.asList(headerLine, invoiceLine, invoiceLine2),result);
+
     }
 
 
