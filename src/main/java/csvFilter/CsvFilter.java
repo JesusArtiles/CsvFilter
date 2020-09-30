a006efd2ff4fde38339d55c79b7b0a13b1deeadc
JesusArtiles <jesusartilescidoncha@gamil.com>
2020-09-30 12:45:42 +0100

    Bug solving regarding tax fields being able to be decimals

--- src/main/java/csvFilter/CsvFilter.java
+++ src/main/java/csvFilter/CsvFilter.java
@@ -3,6 +3,8 @@ package csvFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.regex.Pattern;
 
 import static java.util.Objects.isNull;
 
@@ -12,9 +14,13 @@ public class CsvFilter {
         result.add(lines.get(0));
         String invoice = lines.get(1);
         String[] fields = invoice.split(",");
-        int ivaField = 4;
-        int igicField = 5;
-        if ((fields[ivaField].isEmpty() || fields[igicField].isEmpty()) && (!(fields[ivaField].isEmpty() && fields[igicField].isEmpty()))){
+        int ivaFieldIndex = 4;
+        int igicFieldIndex = 5;
+        String ivaField = fields[ivaFieldIndex];
+        String igicField = fields[igicFieldIndex];
+        String decimalRegex = "\\d+(\\.\\d+)?";
+        Boolean taxFieldsAreMutuallyExclusive = (ivaField.matches(decimalRegex) || igicField.matches(decimalRegex)) && (!(ivaField.matches(decimalRegex) && igicField.matches(decimalRegex)));
+        if (taxFieldsAreMutuallyExclusive) {
             result.add(lines.get(1));
         }
         return result;
--- src/test/java/CsvFilterShould.java
+++ src/test/java/CsvFilterShould.java
@@ -10,9 +10,10 @@ import static org.junit.Assert.assertEquals;
 
 public class CsvFilterShould {
 
+    String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+
     @Test
     public void allow_for_correct_lines_only(){
-        String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
         String invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,";
 
         List<String> result = new CsvFilter().filter(Arrays.asList(headerLine, invoiceLine));
@@ -23,7 +24,6 @@ public class CsvFilterShould {
 
     @Test
     public void exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive(){
-        String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
         String invoiceLine = "1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,";
 
         List<String> result = new CsvFilter().filter(Arrays.asList(headerLine, invoiceLine));
@@ -32,12 +32,19 @@ public class CsvFilterShould {
 
     @Test
     public void exclude_lines_with_both_tax_fields_empty_as_one_is_required(){
-        String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
         String invoiceLine = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,";
 
         List<String> result = new CsvFilter().filter(Arrays.asList(headerLine, invoiceLine));
         assertEquals(Arrays.asList(headerLine),result);
 
+    }
+
+    @Test
+    public void exclude_lines_with_non_decimal_tax_fields() {
+        String invoiceLine = "1,02/05/2019,1000,810,XYZ,,ACER Laptop,B76430134,";
+
+        List<String> result = new CsvFilter().filter(Arrays.asList(headerLine, invoiceLine));
+        assertEquals(Arrays.asList(headerLine),result);
     }
 
 
